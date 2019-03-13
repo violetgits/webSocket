5173847f0d141aba76ef8139384ec6555c31e4e5
gang <623907705@qq.com>
2019-03-13 13:25:17 +0800

    项目第一次提交

---
+++ src/main/java/com/violet/SpringbootApplication.java
@@ -0,0 +1,34 @@
+package com.violet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * 简要说明. <br>
+ *
+ * <p>
+ * Date-Time：  2019-03-13 10:05
+ * Company: 道云
+ * <p>
+ *
+ * @author wangchao
+ * @version 1.0.0
+ */
+@SpringBootApplication
+public class SpringbootApplication extends SpringBootServletInitializer {
+
+    public SpringbootApplication() {
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringbootApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/violet/controlller/HelloController.java
@@ -0,0 +1,24 @@
+package com.violet.controlller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 简要说明. <br>
+ *
+ * <p>
+ * Date-Time：  2019-03-13 10:27
+ * Company: 道云
+ * <p>
+ *
+ * @author wangchao
+ * @version 1.0.0
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
